ff6ab6f7388938d2dcf561b9969b70475ef11693
widiand <widiand36@gmail.com>
2025-08-24 23:19:11 +0700

    test exec sql

---
+++ sql_test.go
@@ -0,0 +1,22 @@
+package belajargolangdatabase
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES ('joko', 'Joko')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
