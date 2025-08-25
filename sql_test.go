6905428df39d526b85d0f4f7073259e41bc35333
widiand <widiand36@gmail.com>
2025-08-25 08:06:49 +0700

    test query sql

--- sql_test.go
+++ sql_test.go
@@ -20,3 +20,27 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT * FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id: ", id)
+		fmt.Println("Name: ", name)
+	}
+}
